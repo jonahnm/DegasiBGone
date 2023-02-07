ef69403086404db92278265b3743444162bf7a8f
mewki <mewki@DESKTOP-SSVNILD>
2023-02-06 21:24:34 -0500

    Add project files.

---
+++ IslandBuilding/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using HarmonyLib.Tools;
+
+namespace IslandBuilding
+{
+    [BepInPlugin(myGUID,myName,myV)]
+    public class Class1 : BaseUnityPlugin
+    {
+        private const string myGUID = "MJredd.efi.DegasiBGone";
+        private const string myName = "DegasiBGone";
+        private const string myV = "1.0.0";
+        private static readonly Harmony harmony = new Harmony(myGUID);
+        public static ManualLogSource logger;
+        private void Awake()
+        {
+            harmony.PatchAll();
+            Logger.LogInfo("The Degasi on the floating island have been eradicated!");
+            logger = Logger;
+        }
+    }
+}
---
+++ IslandBuilding/Patches.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using BepInEx.Logging;
+using UnityEngine;
+using UWE;
+using System.Runtime.InteropServices;
+
+namespace IslandBuilding
+{
+    internal class Patches
+    {
+        [HarmonyPatch(typeof(ProtobufSerializer))]
+        public static class ProtobufSerializer_Patch
+        {
+            [HarmonyPatch(nameof(ProtobufSerializer.InstantiatePrefabAsync))]
+            [HarmonyPrefix]
+            public static bool InstanatiatePrefabAsync_Prefix(ProtobufSerializer.GameObjectData gameObjectData) {
+                var classId = gameObjectData.ClassId;
+                if (classId == "569f22e0-274d-49b0-ae5e-21ef0ce907ca" || classId == "0e394d55-da8c-4b3e-b038-979477ce77c1" || classId == "99b164ac-dfb4-4a14-b305-8666fa227717") return false;
+                return true;
+            }
+
+        }
+    }
+}
